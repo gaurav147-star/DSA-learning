ff7104550267c0377a1c87d6984193e57fffed12
gaurav147-star <gauravgupta9158@gmail.com>
2022-01-19 19:36:38 +0530

    BT started

---
+++ BinaryTree/BinaryTreeUse.cpp
@@ -0,0 +1,172 @@
+#include <bits/stdc++.h>
+#include "BinaryTreeNode.h"
+using namespace std;
+
+BinaryTreeNode<int> *takeInput()
+{
+    int rootData;
+    cout << "Enter data" << endl;
+    cin >> rootData;
+    if (rootData == -1)
+    {
+        return NULL;
+    }
+
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    BinaryTreeNode<int> *leftChild = takeInput();
+    BinaryTreeNode<int> *rightChild = takeInput();
+    root->left = leftChild;
+    root->right = rightChild;
+    return root;
+}
+
+BinaryTreeNode<int> *takeInputLevelWise()
+{
+    int rootData;
+    cout << "Enter root data" << endl;
+    cin >> rootData;
+    if (rootData == -1)
+    {
+        return NULL;
+    }
+
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+
+    queue<BinaryTreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+
+    while (pendingNodes.size() != 0)
+    {
+        BinaryTreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+
+        cout << "Enter left child of " << front->data << endl;
+        int leftChild;
+        cin >> leftChild;
+        if (leftChild != -1)
+        {
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(leftChild);
+            front->left = child;
+            pendingNodes.push(child);
+        }
+
+        cout << "Enter right child of " << front->data << endl;
+        int rightChild;
+        cin >> rightChild;
+        if (rightChild != -1)
+        {
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(rightChild);
+            front->right = child;
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
+
+void printTree(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << ": ";
+    if (root->left != NULL)
+    {
+        cout << "L " << root->left->data << " ";
+    }
+    if (root->right != NULL)
+    {
+        cout << "R " << root->right->data << " ";
+    }
+    cout << endl;
+
+    printTree(root->left);
+    printTree(root->right);
+}
+
+void printTreeLevelWise(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<BinaryTreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+
+    while (pendingNodes.size() != 0)
+    {
+        BinaryTreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << front->data << ":";
+        if (front->left)
+        {
+            pendingNodes.push(front->left);
+            cout << "L " << front->left->data << " ";
+        }
+        if (front->right)
+        {
+            pendingNodes.push(front->right);
+            cout << "R " << front->right->data << " ";
+        }
+        cout << endl;
+    }
+}
+
+int numNodes(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    return 1 + numNodes(root->left) + numNodes(root->right);
+}
+
+bool isNodePresent(BinaryTreeNode<int> *root, int x)
+{
+    if (root->data == x)
+    {
+        return true;
+    }
+    bool ans = false;
+    if (root->left)
+    {
+        bool xans = isNodePresent(root->left, x);
+        if (xans)
+        {
+            ans = xans;
+        }
+    }
+    if (root->right)
+    {
+        bool xans = isNodePresent(root->right, x);
+        if (xans)
+        {
+            ans = xans;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    // BinaryTreeNode<int> *root = new BinaryTreeNode<int>(1);
+    // BinaryTreeNode<int> *node1 = new BinaryTreeNode<int>(2);
+    // BinaryTreeNode<int> *node2 = new BinaryTreeNode<int>(3);
+    // root->left = node1;
+    // root->right = node2;
+    // BinaryTreeNode<int> *root = takeInput();
+    BinaryTreeNode<int> *root = takeInputLevelWise();
+
+    // printTree(root);
+    printTreeLevelWise(root);
+    // cout << numNodes(root) << endl;
+
+    if (isNodePresent(root, 10))
+    {
+        cout << "true" << endl;
+    }
+    else
+    {
+        cout << "false" << endl;
+    }
+}
